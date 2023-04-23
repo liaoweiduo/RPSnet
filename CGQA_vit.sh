d53c9bad85fcfdbf7179df40a189e69dbb0da19d
liaoweiduo-sustech <11849249@mail.sustech.edu.cn>
2023-04-23 23:58:42 +0800

    Update CGQA_vit.sh

--- CGQA_vit.sh
+++ CGQA_vit.sh
@@ -12,15 +12,19 @@
 for i_n in {0..1509}
 do
   CUDA_VISIBLE_DEVICES=0 python3 cgqa_continual_vit.py 0 $i_n &
-  CUDA_VISIBLE_DEVICES=1 python3 cgqa_continual_vit.py 1 $i_n &
-  CUDA_VISIBLE_DEVICES=2 python3 cgqa_continual_vit.py 2 $i_n &
-  CUDA_VISIBLE_DEVICES=3 python3 cgqa_continual_vit.py 3 $i_n
+  CUDA_VISIBLE_DEVICES=1 python3 cgqa_continual_vit.py 1 $i_n
+  sleep 20
+
+  CUDA_VISIBLE_DEVICES=0 python3 cgqa_continual_vit.py 2 $i_n &
+  CUDA_VISIBLE_DEVICES=1 python3 cgqa_continual_vit.py 3 $i_n
   sleep 20
 
   CUDA_VISIBLE_DEVICES=0 python3 cgqa_continual_vit.py 4 $i_n &
-  CUDA_VISIBLE_DEVICES=1 python3 cgqa_continual_vit.py 5 $i_n &
-  CUDA_VISIBLE_DEVICES=2 python3 cgqa_continual_vit.py 6 $i_n &
-  CUDA_VISIBLE_DEVICES=3 python3 cgqa_continual_vit.py 7 $i_n
+  CUDA_VISIBLE_DEVICES=1 python3 cgqa_continual_vit.py 5 $i_n
+  sleep 20
+
+  CUDA_VISIBLE_DEVICES=0 python3 cgqa_continual_vit.py 6 $i_n &
+  CUDA_VISIBLE_DEVICES=1 python3 cgqa_continual_vit.py 7 $i_n
   sleep 20
 
 done
