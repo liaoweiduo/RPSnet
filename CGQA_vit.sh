4b6e09b535cd34352a0adfe60d1e7a2626e6e463
liaoweiduo-sustech <11849249@mail.sustech.edu.cn>
2023-04-29 22:27:48 +0800

    Update CGQA_vit.sh

--- CGQA_vit.sh
+++ CGQA_vit.sh
@@ -2,11 +2,11 @@
 
 # continual 0...9; sys 10..309; pro 310...609; sub 610..909; non 910..1209; noc 1210..1509
 
-CUDA_VISIBLE_DEVICES=0 python3 cgqa_continual_vit.py 4 2
+#CUDA_VISIBLE_DEVICES=0 python3 cgqa_continual_vit.py 4 2 &
 #CUDA_VISIBLE_DEVICES=1 python3 cgqa_continual_vit.py 5 2 &
 #CUDA_VISIBLE_DEVICES=2 python3 cgqa_continual_vit.py 6 2 &
 #CUDA_VISIBLE_DEVICES=3 python3 cgqa_continual_vit.py 7 2
-sleep 20
+#sleep 20
 
 # for only 2 GPUs
 #for i_n in {0..1509}
