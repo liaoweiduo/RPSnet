c2327b47e42bac1f2668b24522c0ecf46d091b54
liaoweiduo-sustech <11849249@mail.sustech.edu.cn>
2023-05-20 17:45:54 +0800

    up

--- CGQA_vit.sh
+++ CGQA_vit.sh
@@ -12,14 +12,14 @@
 
 #CUDA_VISIBLE_DEVICES=0 python3 cgqa_continual_vit.py 0 3
 
-for i in {4..7}
-do
-  CUDA_VISIBLE_DEVICES=0 python3 cgqa_continual_vit.py $i 4
-done
+#for i in {4..7}
+#do
+#  CUDA_VISIBLE_DEVICES=0 python3 cgqa_continual_vit.py $i 4
+#done
 
-CUDA_VISIBLE_DEVICES=0 python3 cgqa_continual_vit.py 0 5
+#CUDA_VISIBLE_DEVICES=0 python3 cgqa_continual_vit.py 0 5
 
-for i in {0..7}
+for i in {2..7}
 do
   CUDA_VISIBLE_DEVICES=0 python3 cgqa_continual_vit.py $i 6
 done
--- COBJ.sh
+++ COBJ.sh
@@ -32,8 +32,57 @@
 
 
 
-## 10-tasks 3-way
-# continual 0...9; sys 10..309; pro 310...609; non 910..1209; noc 1210..1509
+### 10-tasks 3-way
+## continual 0...9; sys 10..309; pro 310...609; non 910..1209; noc 1210..1509
+#
+##  $1 is lr, $2 is cuda
+## bash COBJ.sh 1e-3
+#
+#CUDA_VISIBLE_DEVICES=$2 python3 cobj_continual.py 0 0 $1
+#
+#CUDA_VISIBLE_DEVICES=$2 python3 cobj_continual.py 0 1 $1
+#
+#for i in {0..7}
+#do
+#  CUDA_VISIBLE_DEVICES=$2 python3 cobj_continual.py $i 2 $1
+#done
+#
+#CUDA_VISIBLE_DEVICES=$2 python3 cobj_continual.py 0 3 $1
+#
+#for i in {0..7}
+#do
+#  CUDA_VISIBLE_DEVICES=$2 python3 cobj_continual.py $i 4 $1
+#done
+#
+#CUDA_VISIBLE_DEVICES=$2 python3 cobj_continual.py 0 5 $1
+#
+#for i in {0..7}
+#do
+#  CUDA_VISIBLE_DEVICES=$2 python3 cobj_continual.py $i 6 $1
+#done
+#
+#CUDA_VISIBLE_DEVICES=$2 python3 cobj_continual.py 0 7 $1
+#
+#for i in {0..7}
+#do
+#  CUDA_VISIBLE_DEVICES=$2 python3 cobj_continual.py $i 8 $1
+#done
+#
+#CUDA_VISIBLE_DEVICES=$2 python3 cobj_continual.py 0 9 $1
+#
+#for i_n in {10..609}   # for fewshot, only once, since use all trained modules when doing inference.
+#do
+#  CUDA_VISIBLE_DEVICES=$2 python3 cobj_continual.py 0 $i_n $1
+#done
+#
+#for i_n in {910..1509}   # for fewshot, only once, since use all trained modules when doing inference.
+#do
+#  CUDA_VISIBLE_DEVICES=$2 python3 cobj_continual.py 0 $i_n $1
+#done
+
+
+## 5-tasks 6-way
+# continual 0...4; sys 5..304; pro 305...604; non 905..1204; noc 1205..1504
 
 #  $1 is lr, $2 is cuda
 # bash COBJ.sh 1e-3
@@ -54,28 +103,12 @@ do
   CUDA_VISIBLE_DEVICES=$2 python3 cobj_continual.py $i 4 $1
 done
 
-CUDA_VISIBLE_DEVICES=$2 python3 cobj_continual.py 0 5 $1
-
-for i in {0..7}
-do
-  CUDA_VISIBLE_DEVICES=$2 python3 cobj_continual.py $i 6 $1
-done
-
-CUDA_VISIBLE_DEVICES=$2 python3 cobj_continual.py 0 7 $1
-
-for i in {0..7}
-do
-  CUDA_VISIBLE_DEVICES=$2 python3 cobj_continual.py $i 8 $1
-done
-
-CUDA_VISIBLE_DEVICES=$2 python3 cobj_continual.py 0 9 $1
-
-for i_n in {10..609}   # for fewshot, only once, since use all trained modules when doing inference.
+for i_n in {5..604}   # for fewshot, only once, since use all trained modules when doing inference.
 do
   CUDA_VISIBLE_DEVICES=$2 python3 cobj_continual.py 0 $i_n $1
 done
 
-for i_n in {910..1509}   # for fewshot, only once, since use all trained modules when doing inference.
+for i_n in {905..1504}   # for fewshot, only once, since use all trained modules when doing inference.
 do
   CUDA_VISIBLE_DEVICES=$2 python3 cobj_continual.py 0 $i_n $1
 done
