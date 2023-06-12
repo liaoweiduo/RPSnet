f755305c0b294ccd8cf25a59bc14223de08e9dcc
liaoweiduo-sustech <11849249@mail.sustech.edu.cn>
2023-06-12 14:38:11 +0800

    Update CGQA_vit.sh

--- CGQA_vit.sh
+++ CGQA_vit.sh
@@ -19,7 +19,7 @@
 #  CUDA_VISIBLE_DEVICES=$2 python3 cgqa_continual_vit.py $i 4 $1
 #done
 #
-#CUDA_VISIBLE_DEVICES=$2 python3 cgqa_continual_vit.py 0 5 $1
+CUDA_VISIBLE_DEVICES=$2 python3 cgqa_continual_vit.py 0 5 $1
 
 for i in {0..2}
 do
